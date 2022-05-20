6aa767d615cd7bc8986d2e51ef92165bf9eda5e3
sijypaul <sijypaul28@gmail.com>
2022-05-20 21:06:28 +0530

    commit

---
+++ src/main/java/com/example/EmployeeManagementSystemSpringbootProject/EmployeeManagementSystemSpringbootProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.EmployeeManagementSystemSpringbootProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSystemSpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSystemSpringbootProjectApplication.class, args);
+		System.out.println("hloooo");
+	}
+
+}
---
+++ src/test/java/com/example/EmployeeManagementSystemSpringbootProject/EmployeeManagementSystemSpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeManagementSystemSpringbootProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemSpringbootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
